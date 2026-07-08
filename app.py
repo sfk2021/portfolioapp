0ea179c370e82d4454278946b189428628cd9af4
sfk2021 <s.f.khan804@gmail.com>
2026-07-08 23:31:28 +0300

    Updated section

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ with col2:
     st.link_button("LinkedIn", "https://www.linkedin.com/in/saba-khan-791061216/")
 
 with col3:
-    st.write("📧 saba.sk.khan15@gmail.com")
+    st.write("📧 Email","saba.sk.khan15@gmail.com")
 
 # -----------------------------------
 # KPI
